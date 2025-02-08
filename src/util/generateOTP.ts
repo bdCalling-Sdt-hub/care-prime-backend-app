d594acc8ec2d20a980717a851b7b5b09cf1878e3
Nadir Hossain <nadirhossain336@gmail.com>
2025-02-08 14:23:51 +0600

    done

--- src/util/generateOTP.ts
+++ src/util/generateOTP.ts
@@ -1,5 +1,5 @@
 const generateOTP = () => {
-  return Math.floor(Math.random() * (999 - 1000 + 1) + 1000);
+  return Math.floor(Math.random() * (9999 - 1000 + 1) + 1000);
 };
 
 export default generateOTP;
