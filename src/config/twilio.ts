bec4dd90a7ff5e41b974f6747e0136207ed19324
Nadir Hossain <nadirhossain336@gmail.com>
2025-04-07 11:37:04 +0600

    feat: :sparkles: added nickname features on the user model

--- src/config/twilio.ts
+++ src/config/twilio.ts
@@ -3,8 +3,8 @@ import config from '../config';
 
 const twilioClient = twilio(config.twilio.accountSid, config.twilio.authToken);
 
-twilioClient.api.accounts
+/* twilioClient.api.accounts
   .list()
   .then(accounts => console.log(accounts))
-  .catch(error => console.error(error));
+  .catch(error => console.error(error)); */
 export default twilioClient;
